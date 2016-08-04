b4a412f9265dbd02d273dbfa01442698b9d28125
mao chao <6233843@gmail.com>
2016-08-04 10:06:58 +0800

    add ajax function

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.feature 'post feature', type: :feature do
   scenario 'create new post' do
     visit '/posts'
-    sleep(5)
+    expect(page).not_to have_content('initialize')
+    expect(page).to have_content('start loading...')
+    expect(page).to have_content('finish loading')
   end
 end
