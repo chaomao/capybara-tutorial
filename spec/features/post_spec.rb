995651a62543e1ab2c34a4a52d5109430a56bcc5
zhen639 <zhen6939@gmail.com>
2016-08-05 16:37:40 +0800

    caizhen, test creating new artical

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,19 +3,15 @@ require 'rails_helper'
 RSpec.feature 'post feature', type: :feature do
   scenario 'create new post' do
     visit '/posts'
-    sleep(1)
-    # expect(page).not_to have_content('initialize')
-    # expect(page).to have_content('start loading...')
-    # expect(page).to have_content('finish loading')
     click_link('New Post')
     fill_in('post_title', with: 'my first post')
-    sleep(1)
     fill_in('post_content', with: 'I wanna be the full-stack dev')
-    sleep(1)
     click_button('Create Post')
-    sleep(1)
+    expect(page).to have_content('Post was successfully created.')
     visit '/posts'
-    sleep(1)
     expect(page).to have_content('my first post')
+    expect(page).to have_selector('.top-10-list')
+    top_articles = find('.top-10-list').all('li').collect(&:text)
+    expect(top_articles).to include('my first post')
   end
 end
